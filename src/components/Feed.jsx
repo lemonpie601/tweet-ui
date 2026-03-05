7e8d9c0247316ab58bb8c92d41a546c68bf72abb
lemonpie601 <valorsida@gmail.com>
2026-03-05 15:34:31 +0900

    Pass likes count to onLike callback in Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,7 +7,7 @@ export default function Feed({ posts, onLike, onDelete }) {
         <Post
           key={p.id}
           post={p}
-          onLike={() => onLike?.(p.id)}
+          onLike={() => onLike?.(p.id, p.likes)}
           onDelete={() => onDelete?.(p.id)}
         />
       ))}
